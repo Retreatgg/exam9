fc525c5afa6bb9747eb3dc9393d4008688aeb10f
Ashimov Argen <retreatpm@icloud.com>
2024-05-25 08:12:52 +0300

    Add .gitignore

--- src/main/java/com/example/exam9/dto/UserCreateDto.java
+++ src/main/java/com/example/exam9/dto/UserCreateDto.java
@@ -1,5 +1,6 @@
 package com.example.exam9.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserCreateDto {
 
+    @NotEmpty
     private String username;
+
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 24, message = "${user.validation.password.size}")
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$",
+            message = "${user.validation.password.pattern}"
+    )
     private String password;
+
 }
---
+++ src/main/resources/db/changelog/initial/010_data.sql
@@ -0,0 +1,4 @@
+insert into AUTHORITIES(AUTHORITY) values ( 'USER' );
+
+insert into USERS(username, email, password, enabled, account_type)
+VALUES ( 'argen', 'retreatpm@icloud.com', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', true, 1)
